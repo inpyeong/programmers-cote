87dbedff3f672adb596a80a69d89208cb6711722
inpyeong <58331907+inpyeong@users.noreply.github.com>
2020-11-23 14:30:03 +0900

    Update 가장 먼 노드.cpp

--- Level_3/가장 먼 노드.cpp
+++ Level_3/가장 먼 노드.cpp
@@ -6,39 +6,31 @@
 
 using namespace std;
 
-bool cmp(int i, int j) {
-    return i > j;
-}
-
 int solution(int n, vector<vector<int>> edge) {
-    vector<vector<int> > adj(n + 1, vector<int>(n + 1, 0));
-    vector<int> discovered(n + 1, -1);
+    vector<int> adj[n+1];
+    for(auto e : edge) {
+        adj[e[0]].push_back(e[1]);
+        adj[e[1]].push_back(e[0]);
+    }
+    vector<int> dist(n+1, 0);
     queue<int> q;
     
-    for(int i = 0; i < edge.size(); ++i) {
-        int u = edge[i][0], v = edge[i][1];
-        adj[u][v] = adj[v][u] = 1;
-    }
-    
-    q.push(1);
-    discovered[1] = 0;
+    int start = 1;
+    dist[start] = 0;
+    q.push(start);
     while(!q.empty()) {
         int here = q.front();
         q.pop();
-        for(int there = 1; there <= adj.size(); ++there) {
-            if(adj[here][there] && discovered[there] == -1) {
+        for(int i = 0; i < adj[here].size(); ++i) {
+            int there = adj[here][i];
+            if(there != start && !dist[there]) {
                 q.push(there);
-                discovered[there] = discovered[here] + 1;
+                dist[there] = dist[here] + 1;
             }
         }
     }
-    
-    sort(discovered.begin(), discovered.end(), cmp);
-    int maxDistance = discovered[0], answer = 0;
-    for(int i = 0; i < n; ++i) {
-      	if(discovered[i] == maxDistance)
-            answer++;
-        else break;
-    }
+    int maxDist = *max_element(dist.begin(), dist.end()), answer = 0;
+    for(int d : dist)
+        if(d == maxDist) answer++;
     return answer;
 }
