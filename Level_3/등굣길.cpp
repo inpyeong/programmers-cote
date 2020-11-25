75a52c27cf1020d39f5b33f5f45bbdfad757a0a3
inpyeong <58331907+inpyeong@users.noreply.github.com>
2020-11-25 16:23:38 +0900

    Update 등굣길.cpp

--- Level_3/등굣길.cpp
+++ Level_3/등굣길.cpp
@@ -4,24 +4,20 @@
 
 using namespace std;
 
-const int INF = 1000000007;
+const int MOD = 1000000007;
 int cache[101][101];
 
-int memoized(int y, int x, const vector<vector<int> >& map) {
+int memoized(int y, int x) {
+    if(y == 0 || x == 0) return 0;
+    if(y == 1 && x == 1) return 1;
     int& ret = cache[y][x];
     if(ret != -1) return ret;
-    if(y < 1 || x < 1) return 0;
-    if(y == 1 && x == 1) 
-        return ret = 1;
-    if(map[y][x] == -1)
-        return ret = 0;
-    return ret = ((memoized(y, x-1, map)) % INF + (memoized(y-1, x, map)) % INF) % INF;
+    return ret = (memoized(y-1, x) % MOD + memoized(y, x-1) % MOD) % MOD;
 }
 
 int solution(int m, int n, vector<vector<int>> puddles) {
     memset(cache, -1, sizeof(cache));
-    vector<vector<int> > map(101, vector<int>(101, 0));
     for(auto p : puddles)
-        map[p[1]][p[0]] = -1;
-    return memoized(n, m, map);
+        cache[p[1]][p[0]] = 0;
+    return memoized(n, m);
 }
