238ef6c2a23ed12eebba7660320710b2e8effb08
inpyeong <58331907+inpyeong@users.noreply.github.com>
2020-11-26 12:24:43 +0900

    Update 거스름돈.cpp

--- Level_3/거스름돈.cpp
+++ Level_3/거스름돈.cpp
@@ -3,15 +3,14 @@
 
 using namespace std;
 
-const int MAX_MONEY = 100, MAX_N = 100000, MOD = 1000000007;
-int dp[MAX_N + 1];
+const int MOD = 1000000007, MAX_N = 100000;
 
 int solution(int n, vector<int> money) {
+    int dp[MAX_N+1] = { 0, };
     dp[0] = 1;
     for(auto m : money)
-        for(int i = 0; i <= n; ++i) {
-            if(i - m < 0) continue;
-            dp[i] = (dp[i] + dp[i - m]) % MOD;
-        }
+        for(int i = 0; i <= n; ++i)
+            if(i >= m)
+                dp[i] = (dp[i] + dp[i-m]) % MOD;
     return dp[n] % MOD;
 }
