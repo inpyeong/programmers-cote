3827dc0b238d371e0c3528101d233d3e51d3c9e0
inpyeong <58331907+inpyeong@users.noreply.github.com>
2020-12-23 11:18:25 +0900

    Update 조이스틱.cpp

--- Level_2/조이스틱.cpp
+++ Level_2/조이스틱.cpp
@@ -8,16 +8,21 @@ int solution(string name) {
     string temp(length, 'A');
     while(true) {
         temp[i] = name[i];
+        // 조이스틱을 위아래로 조작할 때 적게 조작할 수 있는 방향으로
         name[i] - 'A' > 'Z' + 1 - name[i] ? answer += 'Z' + 1 - name[i] : answer += name[i] - 'A';
+        // 조작이 완료되면 반복문 종료
         if(temp == name) break;
+        // 오른쪽, 왼쪽 이동방향 결정
         for(int move = 1; move < length; ++move) {
             int rMove = (i + move) % length;
             int lMove = (i + length - move) % length;
+            // 오른쪽 이동이 빠를 경우 
             if(name[rMove] != temp[rMove]) {
                 i = rMove;
                 answer += move;
                 break;
             }
+            // 왼쪽 이동이 빠를 경우
             else if(name[lMove] != temp[lMove]) {
                 i = lMove;
                 answer += move;
