001e037f0def2e7a200ceda7fe99b1586788254a
inpyeong <58331907+inpyeong@users.noreply.github.com>
2020-12-28 18:50:49 +0900

    Update 구명보트.cpp

--- Level_2/구명보트.cpp
+++ Level_2/구명보트.cpp
@@ -5,17 +5,25 @@
 using namespace std;
 
 int solution(vector<int> people, int limit) {
-    int answer = 0, cnt = 0, begin = 0, end = 1;
-    sort(people.begin(), people.end());
-    while(true) {
-        int first = people[people.size() - end++];
-        cnt++;
-        if(limit - first >= people[begin]) {
-            begin++;
-            cnt++;
+    int answer = 0, cnt = 0, begin = 0, end = 0;
+    // sort(people.begin(), people.end());
+    // while(true) {
+    //     int first = people[people.size() - end++];
+    //     cnt++;
+    //     if(limit - first >= people[begin]) {
+    //         begin++;
+    //         cnt++;
+    //     }
+    //     answer++;
+    //     if(cnt >= people.size()) break;
+    // }
+    sort(people.begin(), people.end(), greater<int>());
+    int n = people.size();
+    for(int i = 0, j = n-1; i <= j; ++i) {
+        if(people[i] + people[j] <= limit) {
+            j--;
         }
         answer++;
-        if(cnt >= people.size()) break;
     }
     return answer;
 }
