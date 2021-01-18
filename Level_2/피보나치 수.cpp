3be7a869277e798ca6d52b58b5bccd59d1fc930d
inpyeong <58331907+inpyeong@users.noreply.github.com>
2021-01-18 18:14:58 +0900

    Update 피보나치 수.cpp

--- Level_2/피보나치 수.cpp
+++ Level_2/피보나치 수.cpp
@@ -12,11 +12,11 @@ int F(int n) {
     if(n == 0 || n == 1) return n;
     int& ret = cache[n];
     if(ret != -1) return ret;
-    return ret = (F(n-1) % MOD + F(n-2) % MOD) % MOD;
+    return ret = (F(n-1) + F(n-2)) % MOD;
 }
 
 int solution(int n) {
     memset(cache, -1, sizeof(cache));
-    int answer = F(n) % MOD;
+    int answer = F(n);
     return answer;
 }
