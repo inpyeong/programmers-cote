6f7f011a646f3e3df2d940bd6be366175558a571
inpyeong <58331907+inpyeong@users.noreply.github.com>
2021-01-22 07:19:04 +0900

    Update 카카오프렌즈 컬러링북.cpp

--- Level_2/카카오프렌즈 컬러링북.cpp
+++ Level_2/카카오프렌즈 컬러링북.cpp
@@ -1,57 +1,42 @@
 #include <vector>
 #include <queue>
-#include <algorithm>
 
 using namespace std;
 
-bool discovered[100][100];
-int M, N;
-int dx[] = { 0, 1, 0, -1 };
-int dy[] = { 1, 0, -1, 0 };
+int dy[] = { 1, 0, -1,  0 };
+int dx[] = { 0, 1,  0, -1 };
 
-int BFS(const vector<vector<int> >& picture, int y, int x) {
-    queue<pair<int, int> > Q;
-    discovered[y][x] = true;
-    Q.push(make_pair(y, x));
-    
-    int color = picture[y][x];
-    int ret = 1;
-    while(!Q.empty()) {
-        int hereY = Q.front().first;
-        int hereX = Q.front().second;
-        Q.pop();
-        for(int i = 0; i < 4; ++i) {
-            int thereY = hereY + dy[i];
-            int thereX = hereX + dx[i];
-            if(thereY < 0 || thereY >= M || thereX < 0 || thereX >= N) continue;
-            if(color == picture[thereY][thereX] && !discovered[thereY][thereX]) {
-                discovered[thereY][thereX] = true;
-                Q.push(make_pair(thereY, thereX));
-                ret++;
-            }
-        }
-    }
-    return ret;
-}
-
-// 전역 변수를 정의할 경우 함수 내에 초기화 코드를 꼭 작성해주세요.
 vector<int> solution(int m, int n, vector<vector<int>> picture) {
-    for(int i = 0; i < m; ++i)
-        for(int j = 0; j < n; ++j)
-            discovered[i][j] = false;
-    M = m;
-    N = n;
     int number_of_area = 0;
     int max_size_of_one_area = 0;
-    
-    for(int y = 0; y < m; ++y)
-        for(int x = 0; x < n; ++x) {
-            if(picture[y][x] && !discovered[y][x]) {
-                max_size_of_one_area = max(max_size_of_one_area, BFS(picture, y, x));
-                number_of_area++;
+    queue<pair<int, int> > q;
+    vector<vector<bool> > discovered(m, vector<bool>(n, 0));
+    for(int i = 0; i < m; ++i) {
+        for(int j = 0; j < n; ++j) {
+            if(!picture[i][j] || discovered[i][j]) continue;
+            number_of_area++;
+            int size_of_one_area = 0;
+            q.push(make_pair(i, j));
+            discovered[i][j] = true;
+            while(!q.empty()) {
+                int y = q.front().first;
+                int x = q.front().second;
+                q.pop();
+                size_of_one_area++;
+                for(int k = 0; k < 4; ++k) {
+                    int ny = y + dy[k];
+                    int nx = x + dx[k];
+                    if(ny >= 0 && ny < m && nx >= 0 && nx < n) {
+                        if(picture[ny][nx] == picture[y][x] && !discovered[ny][nx]) {
+                            q.push(make_pair(ny, nx));
+                            discovered[ny][nx] = true;
+                        }
+                    }
+                }
             }
+            max_size_of_one_area = max(max_size_of_one_area, size_of_one_area);
         }
-    
+    }
     vector<int> answer(2);
     answer[0] = number_of_area;
     answer[1] = max_size_of_one_area;
