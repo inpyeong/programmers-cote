df38b131ba7cd580be3e469e4d5834530951478e
inpyeong <58331907+inpyeong@users.noreply.github.com>
2021-01-21 23:18:19 +0900

    Update 프린터.cpp

--- Level_2/프린터.cpp
+++ Level_2/프린터.cpp
@@ -1,51 +1,25 @@
-#include <iostream>
 #include <string>
 #include <vector>
-#include <queue>
-#include <utility>
+#include <algorithm>
 
 using namespace std;
 
-void fromTmpToList(queue<pair<int, int>>& list, vector<pair<int, int>>& tmp) {
-    for(int i = 0; i < tmp.size(); ++i)
-        list.push(tmp[i]);
-    tmp.clear();
-}
-
 int solution(vector<int> priorities, int location) {
-    queue<pair<int, int>> list;
-    for(int i = 0; i < priorities.size(); ++i) 
-        list.push(make_pair(i, priorities[i]));
-
     int answer = 0;
-    vector<pair<int, int>> tmp;
-
+    int max = *max_element(priorities.begin(), priorities.end());
     while(true) {
-        bool check = true;
-        pair<int, int> firstElement = list.front();
-        list.pop();
-        while(!list.empty()) {
-            if(firstElement.second < list.front().second) {
-                check = false;
-                break;
-            }
-            else {
-                tmp.push_back(list.front());
-                list.pop();
-            }
+        if(priorities.front() < max) {
+            priorities.push_back(priorities.front());
+            priorities.erase(priorities.begin());
         }
-        if(check) {
-            ++answer;
-            if (firstElement.first == location) return answer;
-            fromTmpToList(list, tmp);
-        }        
         else {
-            while(!list.empty()) {
-                tmp.push_back(list.front());
-                list.pop();
-            }
-            fromTmpToList(list, tmp);
-            list.push(firstElement);
+            answer++;
+            priorities.erase(priorities.begin());
+            max = *max_element(priorities.begin(), priorities.end());
+            if(location == 0) break;
         }
+        location--;
+        if(location < 0) location = priorities.size() - 1;
     }
+    return answer;
 }
