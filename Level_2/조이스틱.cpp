23266a82a2ebf84bd07ab9512b737e16c819aab8
inpyeong <58331907+inpyeong@users.noreply.github.com>
2021-01-24 00:04:20 +0900

    Update 조이스틱.cpp

--- Level_2/조이스틱.cpp
+++ Level_2/조이스틱.cpp
@@ -1,33 +1,40 @@
+#include <iostream>
 #include <string>
 #include <vector>
 
 using namespace std;
 
 int solution(string name) {
-    int answer = 0, i = 0, length = name.length();
-    string temp(length, 'A');
+    int answer = 0, pos = 0;
     while(true) {
-        temp[i] = name[i];
-        // 조이스틱을 위아래로 조작할 때 적게 조작할 수 있는 방향으로
-        name[i] - 'A' > 'Z' + 1 - name[i] ? answer += 'Z' + 1 - name[i] : answer += name[i] - 'A';
-        // 조작이 완료되면 반복문 종료
-        if(temp == name) break;
-        // 오른쪽, 왼쪽 이동방향 결정
-        for(int move = 1; move < length; ++move) {
-            int rMove = (i + move) % length;
-            int lMove = (i + length - move) % length;
-            // 오른쪽 이동이 빠를 경우 
-            if(name[rMove] != temp[rMove]) {
-                i = rMove;
-                answer += move;
-                break;
+        // 모두 바꿨다면 루프 종료
+        if(name == string(name.length(), 'A')) break;
+        // 'A'라면 바꿔야 할 알파벳의 위치탐색
+        if(name[pos] == 'A') {
+            int posL = pos, posR = pos;
+            int cntL = 0, cntR = 0;
+            while(name[posL] == 'A') {
+                posL = (posL - 1) == -1 ? name.length() - 1 : posL - 1;
+                cntL++;
             }
-            // 왼쪽 이동이 빠를 경우
-            else if(name[lMove] != temp[lMove]) {
-                i = lMove;
-                answer += move;
-                break;
+            while(name[posR] == 'A') {
+                posR = (posR + 1) == name.length() ? 0 : posR + 1;
+                cntR++;
             }
+            if(cntL < cntR) {
+                pos = posL;
+                answer += cntL;
+            }
+            else {
+                pos = posR;
+                answer += cntR;
+            }
+        }
+        else {
+            int cntU = 'A' - name[pos];
+            int cntD = 'Z' - name[pos] + 1;
+            answer += min(abs(cntU), abs(cntD));
+            name[pos] = 'A';
         }
     }
     return answer;
