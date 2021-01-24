ffc7b2646f0c4410ff0994dc403295f43f23f941
inpyeong <58331907+inpyeong@users.noreply.github.com>
2021-01-24 13:54:33 +0900

    Update 타켓 넘버.cpp

--- Level_2/타켓 넘버.cpp
+++ Level_2/타켓 넘버.cpp
@@ -3,21 +3,18 @@
 
 using namespace std;
 
-int N, T;
+int answer;
 
-void dfs(const vector<int>& numbers, int& answer, int tmp, int cnt) {
-    if(cnt == N) {
-        if(tmp == T) answer++;
+void dfs(const vector<int>& numbers, int target, int idx, int tmp) {
+    if(idx == numbers.size()) {
+        if(tmp == target) answer++;
         return;
     }
-    dfs(numbers, answer, tmp + numbers[cnt], cnt+1);
-    dfs(numbers, answer, tmp - numbers[cnt], cnt+1);
+    dfs(numbers, target, idx+1, tmp - numbers[idx]);
+    dfs(numbers, target, idx+1, tmp + numbers[idx]);
 }
 
 int solution(vector<int> numbers, int target) {
-    int answer = 0;
-    N = numbers.size();
-    T = target;
-    dfs(numbers, answer, 0, 0);
+    dfs(numbers, target, 0, 0);
     return answer;
 }
