e04c10299b56b16ace38d8cb9b4134124c84409f
inpyeong <58331907+inpyeong@users.noreply.github.com>
2021-01-29 01:56:08 +0900

    Update JadenCase 문자열 만들기.cpp

--- Level_2/JadenCase 문자열 만들기.cpp
+++ Level_2/JadenCase 문자열 만들기.cpp
@@ -1,22 +1,13 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 string solution(string s) {
-    for(int i = 0; i < s.size(); ++i) {
-        // 공백은 통과
-        if(s[i] == ' ') continue;
-        // 문장의 첫 문자이거나 각 단어의 첫 문자인 경우 소문자를 대문자로 변경
-        if(i == 0 || (i > 0 && s[i-1] == ' ')) {
-            if(s[i] >= 'a' && s[i] <= 'z')
-                s[i] -= 32;
-        }
-        // 그 외 모든 소문자를 대문자로 변경
-        else {
-            if(s[i] >= 'A' && s[i] <= 'Z')
-                s[i] += 32;
-        }
-    }
+    transform(s.begin(), s.end(), s.begin(), ::tolower);
+    if(s[0] >= 'a' && s[0] <= 'z') s[0] -= 32;
+    for(int i = 1; i < s.length(); ++i) 
+        if(s[i-1] == ' ' && s[i] >= 'a' && s[i] <= 'z') s[i] -= 32;
     return s;
 }
