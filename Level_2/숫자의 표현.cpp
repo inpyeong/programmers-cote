948a354e55a0326012631091e1b61de879bc813f
inpyeong <58331907+inpyeong@users.noreply.github.com>
2021-01-28 17:49:43 +0900

    Update 숫자의 표현.cpp

--- Level_2/숫자의 표현.cpp
+++ Level_2/숫자의 표현.cpp
@@ -5,16 +5,11 @@ using namespace std;
 
 int solution(int n) {
     int answer = 0;
-    for(int i = 1; i <= n; ++i) {
-        int tmp = i, total = 0;
-        while(total <= n) {
-            total += tmp;
-            if(total == n) {
-                answer++;
-                break;
-            }
-            tmp++;
+    for(int i = 1; i <= n; ++i)
+        for(int j = i, sum = 0;; ++j) {
+			sum += j;
+            if(sum == n) answer++;
+            if(sum > n) break;
         }
-    }
     return answer;
 }
