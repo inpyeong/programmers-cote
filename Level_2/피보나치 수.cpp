bdc8f5065874c1db40d166b14d325554928c3f69
inpyeong <58331907+inpyeong@users.noreply.github.com>
2021-01-29 00:35:04 +0900

    Update 피보나치 수.cpp

--- Level_2/피보나치 수.cpp
+++ Level_2/피보나치 수.cpp
@@ -1,15 +1,13 @@
-#include <string>
 #include <cstring>
 #include <vector>
 
 using namespace std;
 
 const int MOD = 1234567;
-
 int cache[100001];
 
 int F(int n) {
-    if(n == 0 || n == 1) return n;
+    if(n <= 1) return n;
     int& ret = cache[n];
     if(ret != -1) return ret;
     return ret = (F(n-1) + F(n-2)) % MOD;
@@ -17,6 +15,5 @@ int F(int n) {
 
 int solution(int n) {
     memset(cache, -1, sizeof(cache));
-    int answer = F(n);
-    return answer;
+    return F(n);
 }
