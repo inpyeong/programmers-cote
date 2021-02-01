46a3a5c625bf059cc020259be331aaac953f54d0
inpyeong <58331907+inpyeong@users.noreply.github.com>
2021-02-01 15:46:13 +0900

    Update 소수 만들기.cpp

--- Level_2/소수 만들기.cpp
+++ Level_2/소수 만들기.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <cmath>
 #include <iostream>
 
 using namespace std;
@@ -6,8 +7,13 @@ using namespace std;
 int answer;
 
 bool isPrime(int n) {
-    for(int i = 2; i * i <= n; ++i)
-        if(n % i == 0) return false;
+    if(n <= 1) return false;
+    if(n == 2) return true;
+    if(n % 2 == 0) return false;
+    int sqrtn = int(sqrt(n));
+    for(int div = 3; div <= sqrtn; div+=2)
+        if(n % div == 0) 
+            return false;
     return true;
 }
 
