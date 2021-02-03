294bb25626a4942e8b39329910dac520904cf7d6
inpyeong <58331907+inpyeong@users.noreply.github.com>
2021-02-03 19:52:54 +0900

    Update 점프와 순간 이동.cpp

--- Level_2/점프와 순간 이동.cpp
+++ Level_2/점프와 순간 이동.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
+
 using namespace std;
 
 int solution(int n) {
     int answer = 0;
-    while(n > 0) {
-        if(n % 2 == 0)
-            n /= 2;
-        else {
+    while(n) {
+        if(n % 2 == 1) {
             n -= 1;
-            n /= 2;
             answer++;
         }
+        else n /= 2;
     }
     return answer;
 }
