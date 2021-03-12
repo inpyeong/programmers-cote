74862602e7df4cf9cdf82b099fba4c9966fd919c
inpyeong <58331907+inpyeong@users.noreply.github.com>
2021-03-12 11:41:49 +0900

    Update [1차] 캐시.cpp

--- Level_2/[1차] 캐시.cpp
+++ Level_2/[1차] 캐시.cpp
@@ -1,36 +1,37 @@
-#include <iostream>
 #include <string>
 #include <vector>
+#include <map>
 #include <algorithm>
 
 using namespace std;
 
 int solution(int cacheSize, vector<string> cities) {
-    if(cacheSize == 0) return 5 * cities.size();
     int answer = 0;
-    // 모든 도시이름 소문자로 변경
-    for(int i = 0; i < cities.size(); ++i)
-        transform(cities[i].begin(), cities[i].end(), cities[i].begin(), ::tolower);
-    
-    vector<pair<int, string> > cache(cacheSize, make_pair(-1, ""));
+    map<string, int> m;
+    map<string, int>::iterator it;
     for(int i = 0; i < cities.size(); ++i) {
-        bool flag = false;
-        for(int j = 0; j < cache.size(); ++j) {
-            // 만약 cache hit일 경우,
-            if(cache[j].second == cities[i]) {
-                answer += 1;
-                cache[j].first = i;
-                flag = true;
-                break;
-            }
+        transform(cities[i].begin(), cities[i].end(), cities[i].begin(), ::tolower);
+        // cache hit
+        if(m.find(cities[i]) != m.end()) {
+            answer += 1;
+            m[cities[i]] = i; 
         }
-        // 만약 cache miss일 경우,
-        if(!flag) {
+        // cache miss
+        else {
             answer += 5;
-            cache[0].first = i;
-            cache[0].second = cities[i];
+            if(m.size() == cacheSize) {
+                string key = "";
+                int val = 100000;
+                for(it = m.begin(); it != m.end(); ++it)
+                    if(it->second < val) {
+                        val = it->second;
+                        key = it->first;
+                    }
+                m.erase(key);
+            }
+            if(cacheSize > 0)
+                m[cities[i]] = i; 
         }
-        sort(cache.begin(), cache.end());
     }
     return answer;
 }
