ca63962fd916fc85cb53623fe177df24278a8f62
inpyeong <58331907+inpyeong@users.noreply.github.com>
2021-03-26 04:35:45 +0900

    Create 실패율.java

---
+++ 2019 KAKAO BLIND RECRUITMENT/실패율.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+class Solution {
+    class Fail {
+        int stage;
+        double rate;
+        Fail(int stage, double rate) {
+            this.stage = stage;
+            this.rate = rate;
+        }
+    }
+    
+    Comparator<Fail> comp = new Comparator<Fail>() {
+        public int compare(Fail a, Fail b) {
+            if(a.rate < b.rate)
+                return 1;
+            else if (a.rate > b.rate)
+                return -1;
+            else {
+                if (a.stage > b.stage)
+                    return 1;
+                else if (a.stage < b.stage)
+                    return -1;
+                else
+                    return 0;
+            }
+        }
+    };
+    
+    public int[] solution(int N, int[] stages) {
+        int[] answer = new int[N];
+        List<Fail> fails = new ArrayList<>();
+        int total = stages.length;
+        
+        // 메모리에 동적으로 생성하는 배열은 0으로 초기화
+        int[] users = new int[N+1];
+        for (int s : stages)
+            users[s-1]++;
+        
+        for (int i = 0; i < N; ++i) {
+            if (users[i] == 0)
+                fails.add(new Fail(i+1, 0));
+            else {
+                fails.add(new Fail(i+1, (double)users[i]/total));
+                total -= users[i];
+            }
+        }
+        
+        Collections.sort(fails, comp);
+        for (int i = 0; i < N; ++i)
+            answer[i] = fails.get(i).stage;
+         
+        return answer;
+    }
+}
