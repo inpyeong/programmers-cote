0fc3cbfd8984cfe55d6b9ab63d68cc33405993ce
inpyeong <58331907+inpyeong@users.noreply.github.com>
2021-03-27 03:13:49 +0900

    Create 무지의 먹방 라이브.java

---
+++ 2019 KAKAO BLIND RECRUITMENT/무지의 먹방 라이브.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+class Solution {
+    class Food {
+        int time;
+        int idx;
+        Food(int t, int i) {
+            time = t;
+            idx = i;
+        }
+    };
+    
+    Comparator<Food> CompTime = new Comparator<Food>() {
+        public int compare(Food a, Food b) {
+            return a.time - b.time;
+        }
+    };
+    
+    Comparator<Food> CompIdx = new Comparator<Food>() {
+        public int compare(Food a, Food b) {
+            return a.idx - b.idx;
+        }
+    };
+    
+    public int solution(int[] food_times, long k) {
+        List<Food> foods = new LinkedList<Food>();
+        int n = food_times.length;
+        for (int i = 0; i < n; ++i)
+            foods.add(new Food(food_times[i], i+1));
+        
+        // foods.sort(CompTime);
+        Collections.sort(foods, CompTime);
+        
+        int pretime = 0;
+        int i = 0;
+        for (Food f : foods) {
+            long diff = f.time - pretime;
+            if (diff != 0) {
+                long spend = diff * n;
+                if(spend <= k){
+                    k -= spend;
+                    pretime = f.time;
+                } else {
+                    k %= n;
+                    // foods.subList(i, food_times.length).sort(CompIdx);
+                    Collections.sort(foods.subList(i, food_times.length), CompIdx);
+                    return foods.get(i+(int)k).idx;
+                }
+            }
+            ++i;
+            --n;
+        }
+        
+        return -1;
+    }
+}
