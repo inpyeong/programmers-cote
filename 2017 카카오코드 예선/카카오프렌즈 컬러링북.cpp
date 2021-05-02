7f591834e912c9aa293aeaaf3c740822ff0f6d50
inpyeong <58331907+inpyeong@users.noreply.github.com>
2021-05-03 01:34:23 +0900

    Create 카카오프렌즈 컬러링북.cpp

---
+++ 2017 카카오코드 예선/카카오프렌즈 컬러링북.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int dy[] = { 1, 0, -1,  0 };
+int dx[] = { 0, 1,  0, -1 };
+
+// 전역 변수를 정의할 경우 함수 내에 초기화 코드를 꼭 작성해주세요.
+vector<int> solution(int m, int n, vector<vector<int>> picture) {
+    int number_of_area = 0;
+    int max_size_of_one_area = 0;
+    queue<pair<int, int> > q;
+    vector<vector<bool> > discovered(m, vector<bool>(n, 0));
+    for(int i = 0; i < m; ++i) {
+        for(int j = 0; j < n; ++j) {
+            if(!picture[i][j] || discovered[i][j]) continue;
+            number_of_area++;
+            int size_of_one_area = 0;
+            q.push(make_pair(i, j));
+            discovered[i][j] = true;
+            while(!q.empty()) {
+                int y = q.front().first;
+                int x = q.front().second;
+                q.pop();
+                size_of_one_area++;
+                for(int k = 0; k < 4; ++k) {
+                    int ny = y + dy[k];
+                    int nx = x + dx[k];
+                    if(ny >= 0 && ny < m && nx >= 0 && nx < n) {
+                        if(picture[ny][nx] == picture[y][x] && !discovered[ny][nx]) {
+                            q.push(make_pair(ny, nx));
+                            discovered[ny][nx] = true;
+                        }
+                    }
+                }
+            }
+            max_size_of_one_area = max(max_size_of_one_area, size_of_one_area);
+        }
+    }
+    vector<int> answer(2);
+    answer[0] = number_of_area;
+    answer[1] = max_size_of_one_area;
+    return answer;
+}
