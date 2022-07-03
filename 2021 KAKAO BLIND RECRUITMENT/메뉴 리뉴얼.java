62ee49f75ce49ed871514977d319443328997a74
inpyeong <58331907+inpyeong@users.noreply.github.com>
2022-07-03 21:34:12 +0900

    Update 메뉴 리뉴얼.java

--- 2021 KAKAO BLIND RECRUITMENT/메뉴 리뉴얼.java
+++ 2021 KAKAO BLIND RECRUITMENT/메뉴 리뉴얼.java
@@ -1,48 +1,58 @@
 import java.util.*;
 
 class Solution {
-    List<Map<String, Integer>> FoodMaps = new ArrayList<>();
-    int[] MaxCnt = new int[11];
-    
-    void comb(char[] str, int pos, StringBuilder candi) {
-        if (pos >= str.length) {
-        	int len = candi.length();
-            if (len >= 2) {
-                int cnt = FoodMaps.get(len).getOrDefault(candi.toString(), 0) + 1;
-                FoodMaps.get(len).put(candi.toString(), cnt);
-                MaxCnt[len] = Math.max(MaxCnt[len], cnt);
-            }
+    String[] Answer;
+    String[] Orders;
+    int[] Course; 
+    int MaxCnt;
+    Map<String, Integer> Comb;
+    PriorityQueue<String> PQ;
+
+    public void comb(String order, int idx, int c, String foods) {
+        if (foods.length() == c) {
+            // System.out.println(foods);
+            
+            char arr[] = foods.toCharArray();
+            Arrays.sort(arr);
+            foods = new String(arr);
+            
+            if (!Comb.containsKey(foods))
+                Comb.put(foods, 0);
+            
+            Comb.put(foods, Comb.get(foods) + 1);
+            MaxCnt = Math.max(MaxCnt, Comb.get(foods));
             return;
         }
         
-        comb(str, pos+1, candi.append(str[pos]));
-        candi.setLength(candi.length()-1);
-        comb(str, pos+1, candi);
+        for (int i = idx; i < order.length(); ++i) {
+            comb(order, i + 1, c, foods + order.charAt(i));
+        }
     }
     
     public String[] solution(String[] orders, int[] course) {
-        for (int i = 0; i < 11; ++i)
-        	FoodMaps.add(new HashMap<String, Integer>());
+        Orders = orders;
+        Course = course;
+        PQ = new PriorityQueue<>();
+        
+		for (int c : course) {
+            Comb = new HashMap<>();
+            MaxCnt = 0;
             
-        for (String str : orders) {
-            char[] arr = str.toCharArray();
-            Arrays.sort(arr);
-            comb(arr, 0, new StringBuilder());
-        }
-        
-        List<String> list = new ArrayList<>();
-        for (int len : course) {
-            for (Map.Entry<String, Integer> entry : FoodMaps.get(len).entrySet()) {
-                if (entry.getValue() >= 2 && entry.getValue() == MaxCnt[len]) {
-                    list.add(entry.getKey());
-                }
+            for (String order : orders) {
+                comb(order, 0, c, "");
             }
+            
+ 			for (String k : Comb.keySet()) {
+                if (Comb.get(k) == MaxCnt && MaxCnt > 1)
+                    PQ.offer(k);
+            }      
         }
-        Collections.sort(list);
+        Answer = new String[PQ.size()];
+        int idx = 0;
+        while (!PQ.isEmpty()) {
+			Answer[idx++] = PQ.poll();
+        }
         
-        String[] answer = new String[list.size()];
-        for (int i = 0; i < list.size(); ++i)
-            answer[i] = list.get(i);
-        return answer;
+        return Answer;
     }
 }
