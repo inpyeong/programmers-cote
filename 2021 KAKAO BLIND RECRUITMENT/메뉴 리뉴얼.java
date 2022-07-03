a41a601df3b679e53e1b3b2146683ce676930b27
inpyeong <58331907+inpyeong@users.noreply.github.com>
2022-07-03 21:37:15 +0900

    Update 메뉴 리뉴얼.java

--- 2021 KAKAO BLIND RECRUITMENT/메뉴 리뉴얼.java
+++ 2021 KAKAO BLIND RECRUITMENT/메뉴 리뉴얼.java
@@ -42,9 +42,9 @@ class Solution {
                 comb(order, 0, c, "");
             }
             
- 	    for (String k : Comb.keySet()) {
-            if (Comb.get(k) == MaxCnt && MaxCnt > 1)
-                PQ.offer(k);
+ 	        for (String k : Comb.keySet()) {
+                if (Comb.get(k) == MaxCnt && MaxCnt > 1)
+                    PQ.offer(k);
             }      
         }
         Answer = new String[PQ.size()];
